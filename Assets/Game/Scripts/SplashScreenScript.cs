7490c161beed5418109fb41f2ca3821498cecfad
Denys Kliuch <denysko.kl@gmail.com>
2016-03-07 19:14:56 +0200

    add scene

---
+++ Assets/Game/Scripts/SplashScreenScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour {
+
+
+	private int startGame = 3;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("```");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		startGame--;
+		if (startGame <= 0) 	
+		{
+			SceneManager.LoadScene ("MainMenu");
+		}
+	}
+}
