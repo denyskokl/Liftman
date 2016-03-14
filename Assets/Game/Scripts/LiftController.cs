00ae04c7dcc9f10b4db116e0e6fb3ad4fca62909
Alex Hedelyuk <hedelyuk.alexandr@gmail.com>
2016-03-14 22:55:18 +0200

    changed lift speed

--- Assets/Game/Scripts/LiftController.cs
+++ Assets/Game/Scripts/LiftController.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LiftController : MonoBehaviour {
+public class LiftController : MonoBehaviour
+{
+  private Vector3 MouseLastPosition;
 
- private Vector3 MouseLastPosition;
-
-   public float speed;
+  public float Speed;
+  public float SpeedWheel;
   [SerializeField] private CameraScript camera;
   private Rigidbody2D _rigidbody2D;
 
@@ -14,36 +15,40 @@ public class LiftController : MonoBehaviour {
     _rigidbody2D = GetComponent<Rigidbody2D>();
   }
 
-   void Update()
-   {
-     if (Application.isEditor)
-     {
-       UpdateTouchInput();
-     }
+  void Update()
+  {
+    if (Application.isEditor)
+    {
+      UpdateTouchInput();
+    }
 
-     // Debug.Log(Input.touchCount);
-     if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-     {
-       Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-       touchDeltaPosition = touchDeltaPosition*speed;
-       TouchMoved(touchDeltaPosition);
-     }
-   }
+    // Debug.Log(Input.touchCount);
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+    {
+      Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+      touchDeltaPosition = touchDeltaPosition*Speed;
+      TouchMoved(touchDeltaPosition);
+    }
+  }
 
 
-   public void UpdateTouchInput()
-   {
-     var d = Input.GetAxis("Mouse ScrollWheel");
-       _rigidbody2D.AddForce(new Vector2(0, speed*d), ForceMode2D.Force);
-   }
+  public void UpdateTouchInput()
+  {
+    var d = Input.GetAxis("Mouse ScrollWheel");
+    _rigidbody2D.AddForce(new Vector2(0, SpeedWheel*d), ForceMode2D.Force);
+  }
 
-   void TouchMoved(Vector2 touch)
-   {
+  void TouchMoved(Vector2 touch)
+  {
     _rigidbody2D.AddForce(new Vector2(0, -touch.y), ForceMode2D.Force);
-   }
+  }
 
   void OnTriggerEnter2D(Collider2D collider)
   {
-    Debug.Log("OnTriggerEnter" + collider.name);
+  }
+
+  void OnTriggerStay2D(Collider2D collider)
+  {
+    Debug.Log(collider.name);
   }
 }
