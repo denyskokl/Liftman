c73a3eba188a672c8161b11e0860991c15448b94
Denys Kliuch <denysko.kl@gmail.com>
2016-03-14 22:46:17 +0200

    add init prefubs with players

--- Assets/Game/Scripts/GameplayController.cs
+++ Assets/Game/Scripts/GameplayController.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
-using JetBrains.Annotations;
+using DG.Tweening;
 
 public class GameplayController : MonoBehaviour
 {
   [SerializeField] private GameObject lift;
   [SerializeField] private Collider2D touchArea;
   private Vector3 MouseLastPosition;
+  [SerializeField] private GameObject _gues;
 
-  public float speed;
 
-
+  void Start()
+  {
+    var gues = Instantiate(_gues);
+    gues.transform.SetParent(gameObject.transform);
+    gues.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+  }
 }
---
+++ Assets/Game/Scripts/GuesController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GuesController : MonoBehaviour
+{
+	void Start()
+	{
+		MoveGues ();
+
+	}
+
+	private void MoveGues()
+	{
+		ChageFlip(true);
+		transform.DOMoveX (-7.8f, 6f, false).OnComplete(Pause);
+	}
+
+	private void Pause()
+	{
+		Invoke ("MoveBack", 1f);
+	}
+
+	private void MoveBack()
+	{
+		ChageFlip(false);
+		transform.DOMoveX (-2f, 6f, false).OnComplete(MoveGues);
+	}
+
+	private bool ChageFlip(bool status)
+	{
+		transform.GetComponent<SpriteRenderer> ().flipX = status;
+		return status;
+	}
+
+}
