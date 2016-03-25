2f210a4b3145d6318464149be13d70cdd332f0ce
Denys Kliuch <denysko.kl@gmail.com>
2016-03-25 22:27:23 +0200

    fix

--- Assets/Game/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Game/Scripts/MainMenu/MainMenuScript.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
 
-public class PlayGameMoving : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
   [SerializeField] private Camera mainCamera;
 
