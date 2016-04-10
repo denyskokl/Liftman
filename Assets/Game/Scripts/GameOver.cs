28368584d8ef3e769841eb9f213133a304d18757
Denys Kliuch <denysko.kl@gmail.com>
2016-04-10 12:36:51 +0300

    fix

--- Assets/Game/Scripts/GameOver.cs
+++ Assets/Game/Scripts/GameOver.cs
@@ -4,29 +4,23 @@ using DG.Tweening;
 
 public class GameOver : MonoBehaviour
 {
-
-
   void Awake()
   {
     LifeManager.OnGameOver += ShowGameOver;
-
   }
 
-   private void ShowGameOver()
-   {
+  private void ShowGameOver()
+  {
+    var panel = transform.GetChild(0).gameObject;
+    panel.transform.localScale = Vector3.zero;
+    panel.transform.DOScale(Vector3.one, 3f);
+    panel.SetActive(true);
+    StartCoroutine(Pause());
+  }
 
-       var panel = transform.GetChild(0).gameObject;
-       panel.transform.localScale = Vector3.zero;
-       panel.transform.DOScale(Vector3.one, 3f);
-       panel.SetActive(true);
-       count ++;
-       StartCoroutine(Pause());
-
-   }
-
-    IEnumerator Pause()
-    {
-      yield return new WaitForSeconds(3.1f);
-      Time.timeScale = 0;
-    }
+  IEnumerator Pause()
+  {
+    yield return new WaitForSeconds(3.1f);
+    Time.timeScale = 0;
+  }
 }
